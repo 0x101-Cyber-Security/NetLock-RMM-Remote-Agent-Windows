23bbc8b678799b3f022302fcd3035dcf9a6130ca
Nico Mak <nico.mak@0x101-cyber-security.de>
2024-09-18 01:34:15 +0200

    Remote file browser
    Indexing now calculates file & directory sizes more user friendly

--- NetLock RMM Remote Agent (Windows)/Helper/IO.cs
+++ NetLock RMM Remote Agent (Windows)/Helper/IO.cs
@@ -20,86 +20,6 @@ namespace NetLock_RMM_Remote_Agent_Windows.Helper
             public DateTime last_modified { get; set; }
         }
 
-        // Get directories from path
-        public static List<File_Or_Directory_Info> Get_Directory_Index(string path)
-        {
-            var directoryDetails = new List<File_Or_Directory_Info>();
-
-            try
-            {
-                DirectoryInfo rootDirInfo = new DirectoryInfo(path);
-
-                // Directories
-                foreach (var directory in rootDirInfo.GetDirectories())
-                {
-                    var dirDetail = new File_Or_Directory_Info
-                    {
-                        name = directory.Name,
-                        path = directory.FullName,
-                        last_modified = directory.LastWriteTime,
-                        size = GetDirectorySizeInGB(directory),
-                        type = "0", // 0 = Directory
-                    };
-
-                    directoryDetails.Add(dirDetail);
-                }
-
-                // Files
-                foreach (var file in rootDirInfo.GetFiles())
-                {
-                    var fileDetail = new File_Or_Directory_Info
-                    {
-                        name = file.Name,
-                        path = file.FullName,
-                        last_modified = file.LastWriteTime,
-                        size = Get_File_Size_In_GB(file.FullName),
-                        type = file.Extension,
-                    };
-
-                    directoryDetails.Add(fileDetail);
-                }
-
-                return directoryDetails;
-            }
-            catch (Exception ex)
-            {
-                Logging.Handler.Error("IO.GetDirectoryDetails", "General error", ex.ToString());
-                return directoryDetails;
-            }
-        }
-
-        // Get files from path
-        /*public static List<File_Detail> GetFiles(string path)
-        {
-            var files = new List<File_Detail>();
-
-            try
-            {
-                DirectoryInfo rootDirInfo = new DirectoryInfo(path);
-
-                foreach (var file in rootDirInfo.GetFiles())
-                {
-                    var fileDetail = new File_Detail
-                    {
-                        Name = file.Name,
-                        Path = file.FullName,
-                        Type = file.Extension,
-                        Size = file.Length,
-                        LastModified = file.LastWriteTime,
-                    };
-
-                    files.Add(fileDetail);
-                }
-
-                return files;
-            }
-            catch (Exception ex)
-            {
-                Logging.Handler.Error("IO.GetFiles", "General error", ex.ToString());
-                return files;
-            }
-        }*/
-
         // Get drives
         public static string Get_Drives()
         {
@@ -126,7 +46,78 @@ namespace NetLock_RMM_Remote_Agent_Windows.Helper
             }
         }
 
-        public static string GetDirectorySizeInGB(DirectoryInfo directory)
+        // Get directories from path
+        public static async Task<List<File_Or_Directory_Info>> Get_Directory_Index(string path)
+        {
+            var directoryDetails = new List<File_Or_Directory_Info>();
+
+            try
+            {
+                DirectoryInfo rootDirInfo = new DirectoryInfo(path);
+
+                // Directories
+                foreach (var directory in rootDirInfo.GetDirectories())
+                {
+                    var dirDetail = new File_Or_Directory_Info
+                    {
+                        name = directory.Name,
+                        path = directory.FullName,
+                        last_modified = directory.LastWriteTime,
+                        size = await Get_Directory_Size(directory),
+                        type = "0", // 0 = Directory
+                    };
+
+                    directoryDetails.Add(dirDetail);
+                }
+
+                // Files
+                foreach (var file in rootDirInfo.GetFiles())
+                {
+                    var fileDetail = new File_Or_Directory_Info
+                    {
+                        name = file.Name,
+                        path = file.FullName,
+                        last_modified = file.LastWriteTime,
+                        size = await Get_File_Size(file.FullName),
+                        type = file.Extension,
+                    };
+
+                    directoryDetails.Add(fileDetail);
+                }
+
+                return directoryDetails;
+            }
+            catch (Exception ex)
+            {
+                Logging.Handler.Error("IO.GetDirectoryDetails", "General error", ex.ToString());
+                return directoryDetails;
+            }
+        }
+
+        public static async Task<string> GetSizeFormatted(long sizeInBytes)
+        {
+            if (sizeInBytes >= 1024 * 1024 * 1024) // Check for GB
+            {
+                double sizeInGB = sizeInBytes / (1024.0 * 1024.0 * 1024.0);
+                return sizeInGB.ToString("F2") + " GB";
+            }
+            else if (sizeInBytes >= 1024 * 1024) // Check for MB
+            {
+                double sizeInMB = sizeInBytes / (1024.0 * 1024.0);
+                return sizeInMB.ToString("F2") + " MB";
+            }
+            else if (sizeInBytes >= 1024) // Check for KB
+            {
+                double sizeInKB = sizeInBytes / 1024.0;
+                return sizeInKB.ToString("F2") + " KB";
+            }
+            else // Bytes
+            {
+                return sizeInBytes.ToString() + " Bytes";
+            }
+        }
+
+        public static async Task<string> Get_Directory_Size(DirectoryInfo directory)
         {
             long size = 0;
 
@@ -139,32 +130,30 @@ namespace NetLock_RMM_Remote_Agent_Windows.Helper
                     size += fi.Length;
                 }
 
-                // Convert size to GB and format to 2 decimal places.
-                double sizeInGB = size / (1024.0 * 1024.0 * 1024.0);
-                return sizeInGB.ToString("F2");
+                // Convert and format size.
+                return await GetSizeFormatted(size);
             }
             catch (Exception ex)
             {
-                Logging.Handler.Error("IO.GetDirectorySizeInGB", "General error", ex.ToString());
-                return "0.00";
+                Logging.Handler.Error("IO.Get_Directory_Size", "General error", ex.ToString());
+                return "0.00 Bytes";
             }
         }
 
-        public static string Get_File_Size_In_GB(string file_path)
+        public static async Task<string> Get_File_Size(string filePath)
         {
             try
             {
-                FileInfo fileInfo = new FileInfo(file_path);
+                FileInfo fileInfo = new FileInfo(filePath);
                 long size = fileInfo.Length;
 
-                // Convert size to GB and format to 2 decimal places.
-                double sizeInGB = size / (1024.0 * 1024.0 * 1024.0);
-                return sizeInGB.ToString("F2");
+                // Convert and format size.
+                return await GetSizeFormatted(size);
             }
             catch (Exception ex)
             {
-                Logging.Handler.Error("IO.Get_File_Size_In_GB", "General error", ex.ToString());
-                return "0.00";
+                Logging.Handler.Error("IO.Get_File_Size", "General error", ex.ToString());
+                return "0.00 Bytes";
             }
         }
 
